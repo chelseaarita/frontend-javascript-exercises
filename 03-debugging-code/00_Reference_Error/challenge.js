fa78e5d7dc838f672fe02e9f7c36fad3c47d1001
Chelsea Arita <chelsea@chelseaarita.com>
2015-03-03 04:01:42 +0000

    Reference Error

--- 03-debugging-code/00_Reference_Error/challenge.js
+++ 03-debugging-code/00_Reference_Error/challenge.js
@@ -1,7 +1,10 @@
-module.exports.hello = function(World) {
-   return ("Hello",  World) 
- };
+// module.exports.hello = function(World) {
+//    return "Hello " + World
+//  };
 
-module.exports.hello = function(Name) {
-	return "Hello " + Name 
+module.exports.hello = function(msg) {
+	return "Hello " + msg
 };
+
+
+
--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,12 +1,18 @@
 module.exports.formLetter = function(firstName, senderName, message) {
+	return "Hello " + firstName + ",\n\n" + message + "\n\nSincerely,\n" + senderName;
 
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+	sliceItAndCombineIt.bigString("This is a Test".substring (0, 4, 5, 7));
+	sliceItAndCombineIt.startA("This is a Test", 0, 1, 4, 6);
+	sliceItAndCombineIt("This is a Test", 8);
+	sliceItAndCombineIt("This is a Test", 0, 14);
+	sliceItAndCombineIt("This is a Test", 14);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
+	'Roses are red'.indexOf('Red')
 
 };
 
